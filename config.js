4fa207b1aaaf40b2f66577317aae9c3470887adf
Namhyeon, Go <gnh1201@gmail.com>
2019-03-10 02:31:50 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
     // Object values are used as project name in the app
     // Don't forget to add the API environment!
     api: {
-      "https://2s.re.kr/_/": "Catswords Data API"
+      "https://catswords.re.kr/_/": "Catswords Data API"
     },
 
     // Allow the user to connect to any API by entering a URL in a text field
